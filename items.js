5b7c17460695c1d11da67f00777696b9a0567120
mttltech <61518938+mttltech@users.noreply.github.com>
2020-03-06 10:31:53 -0500

    Create items.js

---
+++ items.js
@@ -0,0 +1,12 @@
+module.exports  = [
+    {"id":"1","name":"Jasmine","desc":"princess jasmine","movies":["Aladdin"]},
+    {"id":"2","name":"Aladdin","desc":"prince of ababwa","movies":["Aladdin"]},
+    {"id":"3","name":"Genie","desc":"genie of the lamp","movies":["Aladdin"]},
+    {"id":"4","name":"Jafar","desc":"mean guy","movies":["Aladdin"]},
+    {"id":"5","name":"Iago","desc":"talking parrot","movies":["Aladdin"]},
+    {"id":"6","name":"Alice","desc":"in wonderland","movies":["Alice in Wonderland"]},
+    {"id":"7","name":"Belle","desc":"beauty and beast","movies":["Beauty and the Beast"]},
+    {"id":"8","name":"Lumiere","desc":"beauty beast candlestick","movies":["Beauty and the Beast"]},
+    {"id":"9","name":"Cinderella","desc":"cinderella","movies":["Cinderella"]},
+    {"id":"10","name":"Pumbaa","desc":"pumbaaa","movies":["Lion King"]},
+];
